584d2cde00cf3dc9ede84c865a229c57babede53
Muhlis Murat Akay <akaymu@gmail.com>
2020-11-14 14:26:19 +0300

    video 7 - Custom Document Page

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 const About = () => (
   <Layout title="About">
     <p>A Javascript programmer</p>
-    <img src="/static/javascript-logo.png" alt="Javascript" height="200px" />
+    <img src="/public/javascript-logo.png" alt="Javascript" height="200px" />
   </Layout>
 );
 
