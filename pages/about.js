fd40a139aac6fd2ab58d896c7815d27548ad3dbf
Muhlis Murat Akay <akaymu@gmail.com>
2020-11-14 12:20:35 +0300

    video-5 components, styledjsx

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+  <Layout title="About">
+    <p>A Javascript programmer</p>
+    <img src="/static/javascript-logo.png" alt="Javascript" height="200px" />
+  </Layout>
+);
+
+export default About;
---
+++ pages/hireme.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const Hireme = () => (
+  <Layout title="Hire Me">
+    <p>
+      You can hire me at <a href="mailto:akaymu@gmail.com">akaymu@gmail.com</a>
+    </p>
+  </Layout>
+);
+
+export default Hireme;
