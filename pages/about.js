cddc5559c72a3eddea07b228c6eb6495f8651cc2
Muhlis Murat Akay <akaymu@gmail.com>
2020-11-14 15:23:10 +0300

    video-8 fetch data with getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,35 @@
+import { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
+
 import Layout from '../components/Layout';
 
-const About = () => (
-  <Layout title="About">
-    <p>A Javascript programmer</p>
-    <img src="/public/javascript-logo.png" alt="Javascript" height="200px" />
-  </Layout>
-);
+class About extends Component {
+  // state = { user: null };
+
+  static async getInitialProps() {
+    const response = await fetch('https://api.github.com/users/reedbarger');
+    const data = await response.json();
+
+    return { user: data };
+  }
+
+  // componentDidMount() {
+  //   fetch('https://api.github.com/users/reedbarger')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       this.setState({ user: data });
+  //     });
+  // }
+
+  render() {
+    const { user } = this.props;
+    return (
+      <Layout title="About">
+        <p>{user.name}</p>
+        <img src={user.avatar_url} alt="Reed" height="200px" />
+      </Layout>
+    );
+  }
+}
 
 export default About;
